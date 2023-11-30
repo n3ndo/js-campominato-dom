4f10dcb87e8fb764448d70a960f826f2d7b79c04
n3ndo <ferdinandof3lixbagnale@gmail.com>
2023-11-30 17:44:16 +0100

    [UPDATE] added adaptive cells

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-function createCell(num){
+function createCell(num, cells_row){
     const square = document.createElement('div');
     square.classList.add('square');
     square.innerText = num;
-
+    square.style.width = `calc(100% / ${cells_row})`;
+    square.style.height = square.style.width
     return square;
 }
 
@@ -10,8 +11,13 @@ function createNewGame(){
     const grid = document.getElementById('grid');
     grid.innerHTML = '';
 
-    for(let i=1; i<=100; i++){
-        let cell = createCell(i);
+    let cells_numb = 100;
+    let cells_row;
+
+    cells_row = Math.sqrt(cells_numb);
+
+    for(let i=1; i<=cells_numb; i++){
+        let cell = createCell(i, cells_row);
 
         grid.appendChild(cell);
     }
